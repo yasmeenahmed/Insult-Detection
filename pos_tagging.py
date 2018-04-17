bf5052cd1b56ec5c65b2ad736fcbe7f6915168a7
yasmeen ahmed <yeasmeenahmed@gmail.com>
2018-04-17 22:57:21 +0200

    modified POS tagging

--- pos_tagging.py
+++ pos_tagging.py
@@ -9,18 +9,22 @@ import nltk
 from nltk.corpus import state_union
 from nltk.tokenize import PunktSentenceTokenizer
 #unserpervised algo
-train_text = state_union.raw("metamorphosis_clean.txt")
-sample_text = state_union.raw("metamorphosis_clean.txt")
-custom_sent _tokenizer = PunktSentenceTokenizer(sample_text)
+train_text = state_union.raw("2005-GWBush.txt")
+sample_text = state_union.raw("2006-GWBush.txt")
+custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
 tokenized = custom_sent_tokenizer.tokenize(sample_text)
 
 def process_content():
     try:
-        for i  in tokenized:
-            word = nltk.word_tokenize(i)
+        for i in tokenized[:5]:
+            words = nltk.word_tokenize(i)
             tagged = nltk.pos_tag(words)
             print(tagged)
-        
+
     except Exception as e:
         print(str(e))
+
+
+process_content()
+
         
