d6176fbf8d4498b9e94737ee1fa2138e873dcbd0
yasmeen ahmed <yeasmeenahmed@gmail.com>
2018-04-18 22:23:41 +0200

    naive bayes

--- pos_tagging.py
+++ pos_tagging.py
@@ -11,6 +11,7 @@ from nltk.tokenize import PunktSentenceTokenizer
 #unserpervised algo
 train_text = state_union.raw("2005-GWBush.txt")
 sample_text = state_union.raw("2006-GWBush.txt")
+
 custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
 tokenized = custom_sent_tokenizer.tokenize(sample_text)
 
