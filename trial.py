07e6265d40e5d7a6ffb1793d17f16e8962231ed8
yasmeen ahmed <yeasmeenahmed@gmail.com>
2018-04-20 15:57:55 +0200

    modified trial to read and write from csv

--- trial.py
+++ trial.py
@@ -8,61 +8,51 @@ import re
 import csv
 from time import time
 
-DIR_PATH = "C://workspace//impermium//"
+DIR_PATH = ""
 
 TRAIN_FILE      = DIR_PATH + "train.csv"
 TEST_SOL_FILE   = DIR_PATH + "test_with_solutions.csv"   # This is also used for training, together with TRAIN_FILE
 BADWORDS_FILE   = DIR_PATH + "badwords.txt"              # attached with submission  
 
-TEST_FILE       = DIR_PATH + "verification.csv"          # set this to the new test file name
+TEST_FILE       = DIR_PATH + "test.csv"          # set this to the new test file name
 PREDICTION_FILE = DIR_PATH + "preds.csv"                 # predictions will be written here 
 
-def normalize(f):
-    f = [x.lower() for x in f]
-    f = [x.replace("\\n"," ") for x in f]        
-    f = [x.replace("\\t"," ") for x in f]        
-    f = [x.replace("\\xa0"," ") for x in f]
-    f = [x.replace("\\xc2"," ") for x in f]
-
-    f = [x.replace(" u "," you ") for x in f]
-    f = [x.replace(" em "," them ") for x in f]
-    f = [x.replace(" da "," the ") for x in f]
-    f = [x.replace(" yo "," you ") for x in f]
-    f = [x.replace(" ur "," you ") for x in f]
+def readCsv(fname, skipFirst=True, delimiter = ","):
+    reader = csv.reader(open(fname),delimiter=delimiter)
     
-    f = [x.replace("won't", "will not") for x in f]
-    f = [x.replace("can't", "cannot") for x in f]
-    f = [x.replace("i'm", "i am") for x in f]
-    f = [x.replace(" im ", " i am ") for x in f]
-    f = [x.replace("ain't", "is not") for x in f]
-    f = [x.replace("'ll", " will") for x in f]
-    f = [x.replace("'t", " not") for x in f]
-    f = [x.replace("'ve", " have") for x in f]
-    f = [x.replace("'s", " is") for x in f]
-    f = [x.replace("'re", " are") for x in f]
-    f = [x.replace("'d", " would") for x in f]
+    rows = []
+    count = 1
+    for row in reader:
+        if not skipFirst or count > 1:      
+            rows.append(row)
+        count += 1
+    return rows
 
-    bwMap = loadBW()
-    for key, value in bwMap.items():
-        kpad = " " + key + " "
-        vpad = " " + value + " "
-        f = [x.replace(kpad, vpad) for x in f]
-    #some code for stemming
-    return f
-#some code for n grams (use tdifvectorizer)
-def ngrams(data , labels, ntrain, mn=1, mx=1, binary = False, stopwords, donorm = False, verbose = True, analyzer_char = False):
-    f = data
-    if donorm:
-        f = normalize(f)
-    
-    ftrain = f[:ntrain]
-    ftest  = f[ntrain:]
-    y_train = labels[:ntrain]
+def run(verbose = True):
     t0 = time()
-       analyzer_type = 'word'
-    if analyzer_char:
-        analyzer_type = 'char'
-        
+
+    train_data = readCsv(TRAIN_FILE)
+    train2_data = readCsv(TEST_SOL_FILE)
+  
+    train_data = train_data + train2_data
+  #  print(train_data)
+    labels  = array([int(x[0]) for x in train_data])
+   # print(labels)  
+    train  = [x[2] for x in train_data]
+
+    test_data = readCsv(TEST_FILE)
+    test_data = [x[2] for x in test_data] 
+    
+    data = train + test_data
+    print(data)
+    n = len(data)
+    ntrain = len(train)
+    
+
+
+
+#some code for n grams (use tdifvectorizer)
+
 
 
 
